50c844f900efc72497f71c879a4857cccf7a44d4
cotsadmin <Park.Jungsang@qualysoft-coe.com>
2025-04-14 16:57:33 +0700

    Commit flask exercise module

---
+++ elastic_data/app.py
@@ -0,0 +1,15 @@
+
+from elastic_data.services.elastic_api import get_indices
+from flask import Flask, render_template_string
+from elastic_data.services.elastic_api import get_indices_web
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    template, html_table = get_indices_web()
+    return render_template_string(template, table=html_table)
+
+if __name__ == '__main__':
+    print("Start app")
+    app.run(debug=True)
--- elastic_data/services/elastic_api.py
+++ elastic_data/services/elastic_api.py
@@ -6,6 +6,7 @@ from requests.auth import HTTPBasicAuth
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from elastic_data.utils.file_utils import generate_dated_excel_filename
 
+
 base_url = "https://10.245.65.35:9200/_cat/indices?format=json&pretty&v"
 
 all_indice_data = []
@@ -41,3 +42,51 @@ def get_indices():
         print(df.head())
         excel_filename = generate_dated_excel_filename(prefix="indice", output_dir=OUTPUT_DIR)
         df.to_excel(excel_filename, index=False, sheet_name="Indice")
+
+def get_indices_web():
+    headers = {"User-Agent": "Mozilla/5.0"}
+
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+    response = requests.get(base_url, auth=HTTPBasicAuth('elastic', 'P@ssword'), headers=headers, verify=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        print(type(data), len(data))
+
+        if isinstance(data, list):
+            for item in data:
+                all_indice_data.append({
+                    "Health": item["health"],
+                    "Status": item["status"],
+                    "Index": item["index"],
+                    "UUID": item["uuid"]
+                })
+        else:
+            print(f"HTTP Error {response.status_code}")
+
+    else:
+        return f"HTTP Error {response.status_code}: {response.text}"
+
+    df = pd.DataFrame(all_indice_data)
+
+    if df.empty:
+        return "No Data. Pleache check API response."
+
+    #HTTP 테이블로 전환
+    html_table = df.to_html(classes='table table-striped', index=False, border=0)
+
+    # 간단한 HTML 템플릿
+    template = """
+    <html>
+    <head>
+        <title>Indice List</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    </head>
+    <body class="container mt-4">
+        <h2>Elasticsearch Indices</h2>
+        {{ table|safe }}
+    </body>
+    </html>
+    """
+
+    return template, html_table
