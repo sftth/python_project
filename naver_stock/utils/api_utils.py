27a51b6d82939ab54e7502459662a2a756ab1c27
sftth318@gmail.com <sftth@naver.com>
2025-04-17 10:56:03 +0700

    update function- append data to excel file

---
+++ naver_stock/utils/api_utils.py
@@ -0,0 +1,24 @@
+
+from datetime import datetime
+
+
+
+def append_data(data, firm, category, name, stock_code):
+    all_stock_data = []
+    if isinstance(data, list):
+        for item in data:
+            all_stock_data.append({
+                "NO": "",
+                "Firm": firm,
+                "Type": category,
+                "Name": name,
+                "Code": stock_code,
+                "Date": datetime.strptime(item["localTradedAt"][:10], "%Y-%m-%d").strftime("%Y.%m.%d"),
+                "Price": float(item["closePrice"].replace(",", "")),  # 쉼표 제거 후 정수 변환
+                "Change": float(item["compareToPreviousClosePrice"].replace(",", "")),  # ✅ 쉼표 제거 후 int 변환
+                "Rate (%)": float(item["fluctuationsRatio"]) * 0.01,  # 등락률은 소수점 필요
+                "Open": float(item["openPrice"].replace(",", "")),
+                "High": float(item["highPrice"].replace(",", "")),
+                "Low": float(item["lowPrice"].replace(",", ""))
+            })
+    return all_stock_data
